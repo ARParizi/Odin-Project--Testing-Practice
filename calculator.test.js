83b6dbb12ce4cc546e2e0108fc588f9c6b4c92b7
ARParizi <52738836+ARParizi@users.noreply.github.com>
2025-07-24 18:04:44 +0330

    Add tests for calculator

---
+++ calculator.test.js
@@ -0,0 +1,22 @@
+import { calculator } from './reverse.js';
+
+
+test('Calculate 1 + 2', () => {
+    expect(calculator.add(1, 2)).toBe(1 + 2);
+});
+
+test('Calculate 0.1 + 0.2', () => {
+    expect(calculator.add(0.1, 0.2)).toBeCloseTo(0.3);
+});
+
+test('Calculate 3 - 2', () => {
+    expect(calculator.subtract(3, 2)).toBe(3 - 2);
+});
+
+test('Calculate 4 / 3', () => {
+    expect(calculator.divide(4, 3)).toBe(4 / 3);
+});
+
+test('Calculate 4 * 3', () => {
+    expect(calculator.multiply(4, 3)).toBe(4 * 3);
+});
