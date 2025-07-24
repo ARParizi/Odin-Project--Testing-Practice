7cc34be5f1916a89541d39ef0723412cb0d0681c
ARParizi <52738836+ARParizi@users.noreply.github.com>
2025-07-24 20:04:13 +0330

    Add implementation for caesarCipher

---
+++ caesarCipher.js
@@ -0,0 +1,19 @@
+export function caesarCipher(str, shift) {
+    return str.split('').map((char) =>
+         isLetter(char) ? cipherChar(char, shift) : char).join('');
+}
+
+function isLetter(char) {
+  return /^[a-zA-Z]$/.test(char);
+}
+
+function isLowerCase(char) {
+  return /^[a-z]$/.test(char);
+}
+
+function cipherChar(char, shift) {
+    const charCode    = char.charCodeAt(0);
+    const baseCode    = isLowerCase(char) ? 97 : 65;
+    const newCharCode = baseCode + (charCode - baseCode + shift) % 26 ;
+    return String.fromCharCode(newCharCode);
+}
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -2,17 +2,17 @@ import { caesarCipher } from './caesarCipher.js';
 
 
 test('Cipher abCdEf, shift by 2', () => {
-    expect(caesarCipher('abCdEf')).toBe('cdEfGh');
+    expect(caesarCipher('abCdEf', 2)).toBe('cdEfGh');
 });
 
 test('Cipher xyz, shift by 3', () => {
-    expect(caesarCipher('xyz')).toBe('abc');
+    expect(caesarCipher('xyz', 3)).toBe('abc');
 });
 
 test('Cipher HeLLo, shift by 3', () => {
-    expect(caesarCipher('HeLLo')).toBe('KhOOr');
+    expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
 });
 
 test('Cipher Hello, World!, shift by 3', () => {
-    expect(caesarCipher('Hello, World!')).toBe('Khoor, Zruog!');
+    expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
 });
